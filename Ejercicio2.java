3819dee78c9508d521b7106e76ab20d7d3d0493f
AlonsoSMdV <asanmor011004@gmail.com>
2022-10-11 13:47:19 +0200

    New Commit

---
+++ Ejercicio2.java
@@ -0,0 +1,6 @@
+public class Ejercicio2 {
+    public static void main(String[] args){
+        String nombre = "Alonso Sánchez Moreno de Vega";
+        System.out.print(nombre);
+    }
+}
---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args){
+        String nombre = "Alonso Sánchez Moreno de Vega";
+        String direccion = "Calle Eneas/58";
+        String telefono = "681634035";
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+    }
+}
